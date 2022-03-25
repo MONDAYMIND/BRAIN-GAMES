99ed85850b3c0e0225a56ec6e2823dcb5335a7c9
Анна Стексова <anya-steksova@mail.ru>
2022-03-25 16:13:14 +1000

    add eslint, gitactions, codeclimate 2part

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import {greetByName} from '../src/cli.js';
+import greetByName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetByName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+const greetByName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default greetByName;
