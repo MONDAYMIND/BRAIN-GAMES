b8a338c0a30bb57d161dd9bb13cecca7ae7d6040
Анна Стексова <anya-steksova@mail.ru>
2022-03-26 15:54:30 +1000

    5 step

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import parityCheck from '../src/paritycheck.js';
+
+console.log('Welcome to the Brain Games!');
+
+parityCheck();
---
+++ src/paritycheck.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+
+const checkParity = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+const parityCheck = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const numbersForParityCheck = [15, 6, 7];
+
+  for (let i = 0; i < numbersForParityCheck.length; i += 1) {
+    console.log(`Question: ${numbersForParityCheck[i]}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === checkParity(numbersForParityCheck[i])) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was '${checkParity(numbersForParityCheck[i])}'.\nLet's try again, ${name}!`);
+      return 'failure';
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+  return 'failure';
+};
+
+export default parityCheck;
